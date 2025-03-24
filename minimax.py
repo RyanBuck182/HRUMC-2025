5fd9878d40802ef1c88fd4775b745ada53adf3e2
ryanbuck182@ <ryanbuck182@gmail.com>
2025-03-23 22:47:20 -0400

    Simplify minimax example

--- minimax.py
+++ minimax.py
@@ -2,124 +2,90 @@ from tree import GraphVizTree
 
 
 def main():
-    minimax = GraphVizTree("r", resolution=192)
+    minimax = GraphVizTree("R", resolution=192,
+                           output_folder="output/minimax")
+    step = minimax.sequencer("minimax")
 
     # Creating the tree
-    minimax.add_edge("r", "a")
-    minimax.add_edge("r", "b")
-    minimax.add_edge("r", "c")
-    minimax.add_edge("a", "a1")
-    minimax.add_edge("a", "a2")
-    minimax.add_edge("b", "b1")
-    minimax.add_edge("b", "b2")
-    minimax.add_edge("c", "c1")
-    minimax.add_edge("c", "c2")
+    minimax.add_edge("R", "A")
+    minimax.add_edge("R", "B")
+    minimax.add_edge("A", "A1")
+    minimax.add_edge("A", "A2")
+    minimax.add_edge("B", "B1")
+    minimax.add_edge("B", "B2")
 
     # Running through minimax and outputting it
-    minimax.visualize_graph_sequential("minimax")
+    step()
 
-    minimax.select("r")
-    minimax.visualize_graph_sequential("minimax")
+    minimax.select("R")
+    step()
 
-    # Branch A
-    minimax.select("a")
-    minimax.visualize_graph_sequential("minimax")
+    minimax.select("A")
+    step()
 
-    minimax.select("a1")
-    minimax.visualize_graph_sequential("minimax")
+    minimax.select("A1")
+    step()
 
-    minimax.set_value("a1", 15)
-    minimax.visualize_graph_sequential("minimax")
+    minimax.set_value("A1", 8)
+    step()
 
-    minimax.select("a")
-    minimax.visualize_graph_sequential("minimax")
+    minimax.select("A")
+    step()
 
-    minimax.set_value("a", 15)
-    minimax.visualize_graph_sequential("minimax")
+    minimax.set_value("A", 8)
+    step()
 
-    minimax.select("a2")
-    minimax.visualize_graph_sequential("minimax")
+    minimax.select("A2")
+    step()
 
-    minimax.set_value("a2", 8)
-    minimax.visualize_graph_sequential("minimax")
+    minimax.set_value("A2", -6)
+    step()
 
-    minimax.select("a")
-    minimax.visualize_graph_sequential("minimax")
+    minimax.select("A")
+    step()
 
-    minimax.set_value("a", 8)
-    minimax.visualize_graph_sequential("minimax")
+    minimax.set_value("A", -6)
+    step()
 
-    minimax.select("r")
-    minimax.visualize_graph_sequential("minimax")
+    minimax.select("R")
+    step()
 
-    minimax.set_value("r", 8)
-    minimax.visualize_graph_sequential("minimax")
+    minimax.set_value("R", -6)
+    step()
 
-    # Branch B
-    minimax.select("b")
-    minimax.visualize_graph_sequential("minimax")
+    minimax.select("B")
+    step()
 
-    minimax.select("b1")
-    minimax.visualize_graph_sequential("minimax")
+    minimax.select("B1")
+    step()
 
-    minimax.set_value("b1", 12)
-    minimax.visualize_graph_sequential("minimax")
+    minimax.set_value("B1", 4)
+    step()
 
-    minimax.select("b")
-    minimax.visualize_graph_sequential("minimax")
+    minimax.select("B")
+    step()
 
-    minimax.set_value("b", 12)
-    minimax.visualize_graph_sequential("minimax")
+    minimax.set_value("B", 4)
+    step()
 
-    minimax.select("b2")
-    minimax.visualize_graph_sequential("minimax")
+    minimax.select("B2")
+    step()
 
-    minimax.set_value("b2", 13)
-    minimax.visualize_graph_sequential("minimax")
+    minimax.set_value("B2", 7)
+    step()
 
-    minimax.select("b")
-    minimax.visualize_graph_sequential("minimax")
+    minimax.select("B")
+    step()
 
-    minimax.select("r")
-    minimax.visualize_graph_sequential("minimax")
+    minimax.select("R")
+    step()
 
-    minimax.set_value("r", 12)
-    minimax.visualize_graph_sequential("minimax")
-
-    # Branch C
-    minimax.select("c")
-    minimax.visualize_graph_sequential("minimax")
-
-    minimax.select("c1")
-    minimax.visualize_graph_sequential("minimax")
-
-    minimax.set_value("c1", 7)
-    minimax.visualize_graph_sequential("minimax")
-
-    minimax.select("c")
-    minimax.visualize_graph_sequential("minimax")
-
-    minimax.set_value("c", 7)
-    minimax.visualize_graph_sequential("minimax")
-
-    minimax.select("c2")
-    minimax.visualize_graph_sequential("minimax")
-
-    minimax.set_value("c2", 3)
-    minimax.visualize_graph_sequential("minimax")
-
-    minimax.select("c")
-    minimax.visualize_graph_sequential("minimax")
-
-    minimax.set_value("c", 3)
-    minimax.visualize_graph_sequential("minimax")
-
-    minimax.select("r")
-    minimax.visualize_graph_sequential("minimax")
+    minimax.set_value("R", 4)
+    step()
 
     minimax.select(None)
-    minimax.select_edge("r", "b")
-    minimax.visualize_graph_sequential("minimax")
+    minimax.select_edge("R", "B")
+    step()
 
 
 if __name__ == "__main__":
